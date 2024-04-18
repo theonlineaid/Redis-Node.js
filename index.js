7057445de1bd718ad290c25f6286d1a68fd1103f
theonlineaid <nkomorfaruk@gmail.com>
2024-04-18 16:21:09 +0600

    Remove time expire and clean code

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 import express from 'express';
 import axios from 'axios';
-import string from "./rdsa/string.js";
-import list from "./rdsa/list.js";
-import sets from "./rdsa/sets.js";
 import client from './client.js';
 
 
 const app = express();
 app.use(express.json());
 const port = process.env.PORT || 5000;
+
+
 app.get('/', async (req, res) => {
     try {
         // Check if data is cached in client
@@ -25,7 +24,7 @@ app.get('/', async (req, res) => {
         await client.json.set('data', '.', data);
 
         // Set expiration for the cached data
-        await client.expire('data', 50);
+        // await client.expire('data', 50);
 
         // Send fetched JSON data to client
         return res.json(data);
@@ -36,23 +35,4 @@ app.get('/', async (req, res) => {
 });
 
 
-// app.get('/', async (req, res) => {
-//     const cachValues = await redis.get('data')
-    
-//     if (cachValues) {
-//         return res.send(cachValues);
-//     }
-    
-//     const {data} = await axios.get("https://dummyjson.com/products");
-
-//     await redis.set('data', data);
-//     await redis.expire('data', 50);
-
-//     return res.send(data);
-// })
-
 app.listen(port,  () => console.log(`server run ${port}..`));
-
-// string()
-// list();
-// sets();
